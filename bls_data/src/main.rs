a22db38fd4e67e42d870a3bf05827c1fc03a7d28
david doria <dor15004@byui.edu>
2022-05-31 18:48:17 -0600

    Reset files

--- bls_data/src/main.rs
+++ bls_data/src/main.rs
@@ -1,3 +1,29 @@
+/************************************************************
+ * Bureau of Labor Statistics Data Analysis
+ * by David Doria
+ * 
+ * Retrieves data from the BLS api and performs statistical
+ * analysis on it
+ */
+
+use reqwest;
+
+
 fn main() {
-    println!("Hello, world!");
+    print!("######################################################\n");
+    print!("#  Bureau of Labor Statistics Data Analysis Program  #\n");
+    print!("#                  by David Doria                    #\n");
+    print!("######################################################\n");
+    print!("\n");
+
+    get_data();
+}
+
+// Get the data from an API
+async fn get_data() {
+    let body = client
+        .get("https://httpbin.org/ip")
+        .send()
+        .await?;
+    println!("body = {:?}", body);
 }
